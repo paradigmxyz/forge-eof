1f4c8fa834c52763d2a0af922f506bc6b4306f60
Federico Gimenez <federico.gimenez@gmail.com>
2024-05-25 12:33:31 +0200

    enable solc entrypoint scripts logging

--- solc-eof.sh
+++ solc-eof.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/usr/local/bin/solc --via-ir --experimental-eof-version --optimize --ir-optimized "$@"
+echo "$@" >> /app/foundry/solc-eof-args.log
+
+exec /usr/local/bin/solc --via-ir --experimental-eof-version --optimize --ir-optimized "$@" 2>> /app/foundry/solc-eof-error.log
--- solc-legacy.sh
+++ solc-legacy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/usr/local/bin/solc --via-ir --optimize --ir-optimized "$@"
+echo "$@" >> /app/foundry/solc-legacy-args.log
+
+exec /usr/local/bin/solc --via-ir --optimize --ir-optimized "$@" 2>> /app/foundry/solc-legacy-error.log
