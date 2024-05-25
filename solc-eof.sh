086c443956891be4f06cb3c0ab4d901c43808d21
Federico Gimenez <federico.gimenez@gmail.com>
2024-05-25 17:58:13 +0200

    removed unused files

--- solc-eof.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "$@" >> /app/foundry/logs/solc-eof-args.log
-
-exec /usr/local/bin/solc --experimental-eof-version "$@" 2>> /app/foundry/logs/solc-eof-error.log
--- solc-legacy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "$@" >> /app/foundry/logs/solc-legacy-args.log
-
-exec /usr/local/bin/solc "$@" 2>> /app/foundry/logs/solc-legacy-error.log
